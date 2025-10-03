3e96e86bdff95ddab00006d1e958d07dcd7d1c34
GihuguKim <kimgyannag1@gmail.com>
2025-10-03 15:34:08 +0200

    Add files via upload

---
+++ Oparators.py
@@ -0,0 +1,17 @@
+num1 = 45
+num2 = 3
+print("number 1:",num1)
+print("number 2:",num2)
+print("Addition:",num1 + num2)
+print("Difference:",num1 - num2)
+print("product:",num1 * num2)
+print("Division:",num1 / num2)
+print("Floor Division:",num1 // num2)
+print("Modulus operation:",num1 % num2)
+print("square:",num2**2)
+print("square root:",num1**0.5)
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+result = num1/2+num2**2+10
+print("Result of given equation is:", result)
